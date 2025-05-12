92df8e3fdf7d5bd2637622a993e76d5622854d4b
Tanmoy <tanmoychakraborty0041383@gmail.com>
2025-05-12 23:37:37 +0530

    3nd commit

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(process.env.port, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Server listening on port - ${port}`)
 })
