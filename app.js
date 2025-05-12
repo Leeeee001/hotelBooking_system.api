97f7d69f5a9d37bdad21497fb1363e897ef26b56
Tanmoy <tanmoychakraborty0041383@gmail.com>
2025-05-13 00:47:43 +0530

    server create

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const port = 3000
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = 4000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server listening on port - ${port}`)
-})
+  console.log(`Server listening on port - ${port}`);
+});
