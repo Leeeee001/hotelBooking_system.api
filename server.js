d5e5d1d2daa4498e7c447fa4fef909bad0568f8d
Tanmoy <tanmoychakraborty0041383@gmail.com>
2025-05-13 16:39:53 +0530

    server create

---
+++ server.js
@@ -0,0 +1,15 @@
+// server 
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// app.get("/", (req, res) => {
+//   res.send(`<h2>server is running....</h2>`);
+// });
+
+app.listen(port, () => {
+  console.log(`Server listening on ${port}`);
+});
+
