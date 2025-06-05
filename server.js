02ec6c52ab1db2c2d160239095de8df0c284f362
Tanmoy <tanmoychakraborty0041383@gmail.com>
2025-06-05 22:32:11 +0530

    add email templates

--- server.js
+++ server.js
@@ -7,12 +7,20 @@ const app = express();
 dbConnect(); // database connection....
 const port = process.env.PORT || 5000;
 
+
 // app.get("/", (req, res) => {
 //   res.send(`<h2>server is running....</h2>`);
 // });
 
+
+
+// port listening....
 app.listen(port, () => {
   console.log(`Server listening on localhost:${port}`);
 });
+
+
+
+
 
 
