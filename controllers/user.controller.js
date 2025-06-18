722a34c6e6bcdeaca19ecc3e452c3cabbe1435c4
Tanmoy <tanmoychakraborty0041383@gmail.com>
2025-06-18 22:43:37 +0530

    added user's features(not tested)

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,7 +1,9 @@
 const User = require("../models/users.model");
 const Room = require("../models/rooms.model");
 const RoomAvailability = require("../models/roomAvailability.model");
-const Hotel = require("../models/hotels.model");
+// const Hotel = require("../models/hotels.model");
+const Booking = require("../models/booking.model");
+
 
 
 // get own profile
@@ -123,12 +125,178 @@ const getRoomDetails = async (req, res) => {
     }
 };
 
+// booking rooms
+const bookRoom = async (req, res) => {
+    try {
+        const { roomId, checkInDate, checkOutDate } = req.body;
+        const userId = req.user._id;
+
+        if (!roomId || !checkInDate || !checkOutDate) {
+            return res.status(400).json({ error: "roomId, checkInDate, and checkOutDate are required" });
+        }
+
+        const start = new Date(checkInDate);
+        const end = new Date(checkOutDate);
+
+        if (start >= end) {
+            return res.status(400).json({ error: "Check-out must be after check-in" });
+        }
+
+        const room = await Room.findById(roomId);
+        if (!room) return res.status(404).json({ error: "Room not found" });
+
+        // Create date range list
+        const dateList = [];
+        let current = new Date(start);
+        while (current < end) {
+            dateList.push(new Date(current));
+            current.setDate(current.getDate() + 1);
+        }
+
+        // Check all days are available
+        const availabilities = await RoomAvailability.find({
+            room_id: roomId,
+            date: { $in: dateList }
+        });
+
+        if (availabilities.length !== dateList.length) {
+            return res.status(400).json({ error: "Room not available for entire duration" });
+        }
+
+        const notAvailable = availabilities.some((day) => day.is_Booked);
+        if (notAvailable) {
+            return res.status(400).json({ error: "Some dates are already booked" });
+        }
+
+        // Mark them as booked
+        for (let day of availabilities) {
+            day.is_Booked = true;
+            await day.save();
+        }
+
+        // Create Booking
+        const totalDays = dateList.length;
+        const totalCost = room.price * totalDays;
+
+        const booking = new Booking({
+            userId,
+            roomId,
+            checkInDate: start,
+            checkOutDate: end,
+            totalAmount: totalCost,
+            status: "confirmed",
+        });
+
+        await booking.save();
+
+        res.status(201).json({
+            message: "Room booked successfully",
+            booking,
+        });
+
+    } catch (err) {
+        return res.status(500).json({ error: err.message });
+    }
+};
+
+// get all bookings
+const getMyBookings = async (req, res) => {
+    try {
+        const bookings = await Booking.find({ userId: req.user._id })
+            .populate({
+                path: "roomId",
+                select: "room_number type price",
+                populate: {
+                    path: "hotelId",
+                    select: "name location"
+                }
+            })
+            .sort({ createdAt: -1 });
+
+        res.status(200).json({
+            message: "Booking history fetched successfully",
+            bookings,
+        });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+// cancle Bookings
+const cancelBooking = async (req, res) => {
+    try {
+        const { bookingId } = req.params;
+        const userId = req.user._id;
+
+        const booking = await Booking.findOne({
+            _id: bookingId,
+            userId,
+            status: "confirmed",
+        });
+
+        if (!booking) {
+            return res.status(404).json({ error: "Booking not found or already cancelled" });
+        }
+
+        const now = new Date();
+        const checkIn = new Date(booking.checkInDate);
+
+        if (checkIn <= now) {
+            return res.status(400).json({ error: "Cannot cancel past or active bookings" });
+        }
+
+        // Free up dates in RoomAvailability
+        const dateList = [];
+        let current = new Date(booking.checkInDate);
+        while (current < new Date(booking.checkOutDate)) {
+            dateList.push(new Date(current));
+            current.setDate(current.getDate() + 1);
+        }
+
+        await RoomAvailability.updateMany(
+            {
+                room_id: booking.roomId,
+                date: { $in: dateList },
+            },
+            { is_Booked: false }
+        );
+
+        booking.status = "cancelled";
+        await booking.save();
+
+        res.status(200).json({ message: "Booking cancelled successfully" });
+
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+// delete user
+const deleteAccount = async (req, res) => {
+    try {
+        const userId = req.user._id;
+
+        const user = await User.findById(userId);
+
+        if (!user) return res.status(404).json({ error: "User not found" });
+
+        if (user.is_deleted) {
+            return res.status(400).json({ error: "Account already deleted" });
+        }
+
+        user.is_deleted = true;
+        user.is_active = false;
+
+        await user.save();
+
+        res.status(200).json({ message: "Account deleted successfully" });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
 
 
 
+module.exports = { getProfile, updateProfile, getAvailableRooms, getRoomDetails, bookRoom, getMyBookings, cancelBooking, deleteAccount }
 
 
-
-
-
-module.exports = { getProfile, updateProfile, getAvailableRooms, getRoomDetails, }
--- routes/user.route.js
+++ routes/user.route.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const { authenticate, authorizeRoles } = require("../middlewares/auth.middleware");
-const { getProfile, updateProfile, getAvailableRooms, getRoomDetails } = require("../controllers/user.controller");
+const { getProfile, updateProfile, getAvailableRooms, getRoomDetails, bookRoom, getMyBookings, cancelBooking, deleteAccount } = require("../controllers/user.controller");
 
 
 
@@ -10,6 +10,11 @@ router.get("/profile", authenticate, authorizeRoles("user"), getProfile);
 router.put("/profile", authenticate, authorizeRoles("user"), updateProfile);
 router.get("/get-rooms", authenticate, authorizeRoles("user"), getAvailableRooms);
 router.get("/room/:room_id", authenticate, authorizeRoles("user"), getRoomDetails);
+router.post("/book-room", authenticate, authorizeRoles("user"), bookRoom);
+router.get("/bookings", authenticate, authorizeRoles("user"), getMyBookings);
+router.put("/cancel-booking/:bookingId", authenticate, authorizeRoles("user"), cancelBooking);
+router.delete("/delete-account", authenticate, authorizeRoles("user"), deleteAccount);
+
 
 
 module.exports = router;
